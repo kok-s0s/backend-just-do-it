3880af5e68bb0f070ddbc213e186681c389d8fde
kok-s0s <2694308562@qq.com>
2022-05-19 10:59:52 +0800

    feat: add getQuote function

--- src/quotes/quotes.controller.ts
+++ src/quotes/quotes.controller.ts
@@ -1,25 +1,12 @@
-import { Controller, Get, Param, Redirect, Req } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { QuotesService } from './quotes.service';
-import { Request } from 'express';
 
 @Controller('quotes')
 export class QuotesController {
   constructor(private readonly quotesService: QuotesService) {}
 
-  @Get('today')
-  getTodayQuote(): any {
-    return this.quotesService.getTodayQuote();
-  }
-
-  @Get('findAll')
-  @Redirect('https://nestjs.com', 301)
-  findAll(): string {
-    return 'test';
-  }
-
-  @Get(':id')
-  findOne(@Param() params): string {
-    console.log(params.id);
-    return `This action returns a #${params.id} cat`;
+  @Post('one')
+  getQuote(@Body() data: { quotesID: Array<{ id: string }> }): any {
+    return this.quotesService.getQuote(data);
   }
 }
--- src/quotes/quotes.service.ts
+++ src/quotes/quotes.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const getQuoteForMongoDB = async (data: {
+  quotesID: Array<{ id: string }>;
+}) => {
+  const quotes: Array<{ id: string }> = await prisma.quote.findMany();
+  let rand = Math.floor(Math.random() * quotes.length);
+  let flag = true;
+
+  if (quotes.length === data.quotesID.length) {
+    flag = false;
+  } else {
+    while (data.quotesID.includes(quotes.slice(rand, rand + 1)[0]))
+      rand = Math.floor(Math.random() * quotes.length);
+  }
+
+  return flag ? quotes.slice(rand, rand + 1)[0] : { over: true };
+};
 
 @Injectable()
 export class QuotesService {
   getTodayQuote(): any {
     return 'toadyQuote';
+  }
+
+  getQuote(data: { quotesID: Array<{ id: string }> }): any {
+    const quote = getQuoteForMongoDB(data)
+      .catch((e) => {
+        throw e;
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
+
+    return quote;
   }
 }
